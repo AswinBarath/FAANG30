5c374a4cfa542e1de38d332ff44575bbb3604657
AswinBarath <aswin2001barath@gmail.com>
2021-10-02 10:08:23 +0530

    #1 Two Sum Brute Force

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # FAANG30 
 
+FAANG 30 problems from Mastering the Coding Interview FAANG - ZTM course
+
 
--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -3,8 +3,27 @@ package arrays;
 public class TwoSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] nums = { 1, 3, 7, 9, 2 };
+		int target = 11;
+		int[] ans = bruteForceSoln(nums, target);
+		for (int num : ans) {
+			System.out.println(num);
+		}
 
 	}
 
+	public static int[] bruteForceSoln(int[] nums, int target) {
+
+		// Two-pointer technique
+		for (int p1 = 0; p1 < nums.length; p1++) {
+			int numberToFind = target - nums[p1];
+			for (int p2 = p1 + 1; p2 < nums.length; p2++) {
+				if (numberToFind == nums[p2]) {
+					return new int[] { p1, p2 };
+				}
+			}
+		}
+
+		return new int[] { -1, -1 };
+	}
 }
